c8c5f23bf3535996d9317d3a6f6abdd6644949c5
Nikita <nikita.sinitsyn.lipetsk@abv.bg>
2023-05-22 17:50:58 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/lessons/springbootbd/weblibrarybd/WebLibraryBdApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springbootbd.weblibrarybd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibraryBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLibraryBdApplication.class, args);
+    }
+
+}
